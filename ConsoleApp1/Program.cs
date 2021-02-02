4254a2d2dabbc50562ab7c85ea32521cebf4d68a
phongnv1106 <76931001+phongnv1106@users.noreply.github.com>
2021-02-02 17:34:19 +0700

    query sql update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,10 +13,30 @@ namespace ConsoleApp1
             // HashCodeFun.HashSHA256();
             #endregion
 
-
+            string rs = HashPassword("abc1A@a");
+            Console.WriteLine(rs);
 
             Console.ReadKey();
            
+        }
+        public static string HashPassword(string password)
+        {
+            password = password + "WZ5WLTPTEYFX6WU47GWD7TPEXJPHRXZD";
+            byte[] salt;
+            byte[] buffer2;
+            if (password == null)
+            {
+                throw new ArgumentNullException("password");
+            }
+            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, 0x10, 0x3e8))
+            {
+                salt = bytes.Salt;
+                buffer2 = bytes.GetBytes(0x20);
+            }
+            byte[] dst = new byte[0x31];
+            Buffer.BlockCopy(salt, 0, dst, 1, 0x10);
+            Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
+            return Convert.ToBase64String(dst);
         }
     }
 }
---
+++ ConsoleApp1/Tam.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Tam
+    {
+        // bỏ phần emailconfirm, phonenumberconfirm, twoFactorEnabled, lockoutEnabled, accessFailedCount
+        //return await WithConnection(async conn =>
+        //{
+        //    var sql = $@"UPDATE Users SET FirstName = @firstName, LastName = @lastName, PasswordHash = @password, StatusId = @statusId,
+        //                Position = @position, Avatar = @avatar, Gender = @gender, BirthDay = @birthDay, PhoneNumber = @phoneNumber,
+        //                Street = @street, City = @city, ProvinceId = @provinceId, CountryId = @countryId, AcademicRankId = @academicRankId,
+        //                AcademicDegreeId = @academicDegreeId, TimeZoneId = @timeZoneId, UserTypeId = @userTypeId,
+        //                OrganizationalStructureId = @organizationalStructureId, Story = @story, 
+        //                WHERE Email = @email";
+        //    return await conn.QueryFirstAsync<int>(sql, new
+        //    {
+        //        firstName = user.FirstName,
+        //        lastName = user.LastName,        
+        //        password = user.Password,
+        //        email = user.Email,
+        //        statusId = user.StatusId,              
+        //        position = user.Position,
+        //        avatar = user.Avatar,
+        //        gender = user.Gender,
+        //        birthDay = user.BirthDay,
+        //        phoneNumber = user.PhoneNumber,
+        //        street = user.Street,
+        //        city = user.City,
+        //        provinceId = user.ProvinceId,
+        //        countryId = user.CountryId,
+        //        academicRankId = user.AcademicRankId,
+        //        academicDegreeId = user.AcademicDegreeId,
+        //        timeZoneId = user.TimeZoneId,
+        //        userTypeId = user.UserTypeId,
+        //        organizationalStructureId = user.OrganizationalStructureId,
+        //        story = user.Story,
+        //    });
+        //});
+    }
+}
